240902e0f3bf118230c45089482ab9a22e33c4ab
Jeremiah Steele <jeremiahsteele@gmail.com>
2018-06-26 00:18:18 -0700

    Start writing make_move method in board.rb

--- W2D2/mancala/lib/board.rb
+++ W2D2/mancala/lib/board.rb
@@ -2,15 +2,17 @@ class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @name1 == name1
+    @name2 == name2
     @cups = Array.new(14){Array.new}
-    @cups.each_with_index do |el, idx|
-      4.times {el << :stone} unless idx == 6 || idx == 13
-    end
+    place_stones
+  end
 
   end
 
   def place_stones
-    # helper method to #initialize every non-store cup with four stones each
+    @cups.each_with_index do |el, idx|
+      4.times {el << :stone} unless idx == 6 || idx == 13
   end
 
   def valid_move?(start_pos)
@@ -19,9 +21,23 @@ class Board
   end
 
   def make_move(start_pos, current_player_name)
+    stones = @cups[start_pos].length
+    @cups[start_pos] = []
+    pos = (start_pos + 1) % 14
+    until stones == 0 do
+      unless (pos == 6 && current_player_name == @name2) || (pos == 13 && current_player_name == @name1)
+        @cups[pos] << :stone
+        stones -= 1
+      end
+      pos = (pos + 1) % 14
+    end
+
+    render
+    next_turn((pos - 1) % 14)
   end
 
   def next_turn(ending_cup_idx)
+
     # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
   end
 
@@ -34,8 +50,17 @@ class Board
   end
 
   def one_side_empty?
+    @cups[0..5].all? {|cup| cup.empty?} || @cups[7..12].all? {|cup| cup.empty?}
   end
 
   def winner
+    case @cups[6].length <=> @cups[13].length
+    when 0
+      return :draw
+    when 1
+      return @name1
+    when -1
+      return @name2
+    end
   end
 end
