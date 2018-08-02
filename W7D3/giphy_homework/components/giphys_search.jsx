f104eddfc3a5f8ca8c02229249a80934728b2eab
Jeremiah Steele <jeremiahsteele@gmail.com>
2018-08-02 07:10:39 -0700

    Complete phase I of Rainbow Routes homework

---
+++ W7D3/giphy_homework/components/giphys_search.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import GiphysIndex from './giphys_index';
+
+import fetchSearchGiphys from '../utils/api_util';
---
+++ W7D4/rainbow_routes/components/red.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Route, Link, NavLink } from 'react-router-dom';
+import Orange from './orange';
+import Yellow from './yellow';
+
+class Red extends React.Component {
+  render() {
+    return(
+      <div>
+        <h2 className="red"></h2>
+        <Route path="/red/orange" component={Orange} />
+        <Route path="/red/yellow" component={Yellow} />
+      </div>
+    );
+  }
+}
+
+export default Red;
